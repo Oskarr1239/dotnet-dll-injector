a77025c1af7dcd4aa0fb7363ff89d63b70cbb674
Bartosz Firyn <bartosz.firyn@alcatel-lucent.com>
2012-08-09 21:34:27 +0200

    Add DLL framework version checker

---
+++ src/main/cs/FrameworkChecker/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+
+namespace FrameworkChecker {
+    
+    class Program {
+        
+        static void Main(string[] args) {
+
+            if (args.Length == 0) {
+                Console.WriteLine("Usage: frmvchk [dllpath]");
+                Console.WriteLine("  [dllpath]   - path to the DLL library to check framework version");
+                return;
+            }
+
+            Assembly a = Assembly.ReflectionOnlyLoadFrom(args[0]);
+            Console.WriteLine(a.ImageRuntimeVersion);
+        }
+    }
+}
---
+++ src/main/cs/FrameworkChecker/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FrameworkChecker")]
+[assembly: AssemblyDescription("Check framework version embedded in managed DLL")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Druzgocące Krasnale z Elbonii")]
+[assembly: AssemblyProduct("FrameworkChecker")]
+[assembly: AssemblyCopyright("Copyright © Druzgocące Krasnale z Elbonii 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0df766b5-867d-4ded-bafa-322cff5b53ab")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
